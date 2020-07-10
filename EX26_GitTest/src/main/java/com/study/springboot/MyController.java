e08bb67739a30d7169bdf19c6029a401a1db6133
user <user@i7C-62>
2020-07-10 10:08:30 +0900

    초기버젼 회원가입 추가 로그인 추가 dqd.jsp 수정

---
+++ EX26_GitTest/src/main/java/com/study/springboot/MyController.java
@@ -0,0 +1,25 @@
+package com.study.springboot;
+
+public class MyController {
+	
+	//Git Test
+	//Git 사용하는 장점
+	//1. 소스 안전 관리
+	//2. 히스토리 관리가능 -수정/추가/삭제/변경 기록 다 남음
+	//3. 팀 작업 용이
+	
+	
+	//clone : 복제(서버저장소를 로컬에 복제함.)
+	//master : 최상단 소스 노드
+	//branch : 가지치기 master-------------------->
+	//							|			  ^ merge
+	//							--> branch ---|
+	
+	//프로젝트소스 폴더
+	//	|			^	
+	//	v commit	|	fetch
+	//로컬 PC 저장소
+	//	|			^
+	//	vpush		|	pull
+	//원격 저장소(Github 서버)
+}
